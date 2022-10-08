c3b3318f0c4eb8b5c60b237e8ce51378e0589ded
Christof <christof_sprenger@hotmail.com>
2022-10-08 08:40:35 -0700

    time

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿
+using System.Diagnostics;
+
 var pdf = CumulativeDistributionFunction.Create(("A", 100), ("B", 100), ("C", 60));
 
 var rng = Random.Shared;
 var counts = new Counter<string>();
+var sw = Stopwatch.StartNew();
 for (int i = 0; i < 10_000_000; i++)
 {
     var p = pdf.Pick(rng.NextDouble());
     counts[p] += 1;
 }
-
+sw.Stop();
 Console.WriteLine("{0:P}", counts);
+Console.WriteLine("{0}", sw.Elapsed);
