838b3ec78c90c692e16d56116c6006d722b10d3d
Christof <christof_sprenger@hotmail.com>
2022-10-08 08:59:31 -0700

    cleanup

--- CumulativeDistributionFunction{K}.cs
+++ CumulativeDistributionFunction{K}.cs
@@ -8,15 +8,16 @@ class CumulativeDistributionFunction<TKey> where TKey : notnull
     }
 
     private double[] bounds;
+
     private TKey[] labels;
 
     public TKey Pick(double val)
     {
         var ix = Array.BinarySearch(bounds, val);
+        //  If value is not found and value is less than one or more elements in array, 
+        // the negative number returned is the bitwise complement of the index of the first 
+        // element that is larger than value. 
         ix = ix < 0 ? ~ix : ix;
         return labels[ix];
     }
 }
-
-
-
