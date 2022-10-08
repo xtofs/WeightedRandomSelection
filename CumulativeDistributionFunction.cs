a79a42e5715dd6b22d92f5383ce6f30a30d1978e
Christof <christof_sprenger@hotmail.com>
2022-10-08 11:36:05 -0700

    fix weighting

--- CumulativeDistributionFunction.cs
+++ CumulativeDistributionFunction.cs
@@ -1,23 +1,19 @@
 static class CumulativeDistributionFunction
 {
-    public static CumulativeDistributionFunction<K> Create<K>(IEnumerable<(K Label, int weight)> items) where K : notnull
+    public static CumulativeDistributionFunction<K> Create<K>(IEnumerable<(K Label, int Weight)> items) where K : notnull
     {
-        var ranges = items.Scan(p => p.weight, 0.0, (a, b) => a + b, (item, a) => (UpperBound: a, item.Label));
+        double sum = items.Sum(p => p.Weight);
+        var ranges = items.Scan(p => p.Weight / sum, 0.0, (a, b) => a + b, (item, a) => (UpperBound: a, item.Label));
 
         return new CumulativeDistributionFunction<K>(ranges);
     }
 
-    public static CumulativeDistributionFunction<K> Create<K>(IEnumerable<(K, double)> items) where K : notnull
+    public static CumulativeDistributionFunction<K> Create<K>(IEnumerable<(K Label, double Weight)> items) where K : notnull
     {
-        var density = new List<(double, K)>();
-        double current = 0;
-        var sum = items.Sum(kvp => kvp.Item2);
-        foreach (var (k, v) in items)
-        {
-            current += ((double)v) / sum;
-            density.Add((current, k));
-        }
-        return new CumulativeDistributionFunction<K>(density);
+        double sum = items.Sum(p => p.Weight);
+        var ranges = items.Scan(p => p.Weight / sum, 0.0, (a, b) => a + b, (item, a) => (UpperBound: a, item.Label));
+
+        return new CumulativeDistributionFunction<K>(ranges);
     }
 
     public static CumulativeDistributionFunction<K> Create<K>(params (K, int)[] items) where K : notnull => Create(items.AsEnumerable());
